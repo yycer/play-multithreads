9e964d4754ff2284fe9cab36f372867d7c1e4842
yaofrankie <15900617310@163.com>
2020-01-19 17:16:35 +0800

    switch test.

---
+++ src/main/java/com/frankie/demo/utils/Status.java
@@ -0,0 +1,11 @@
+package com.frankie.demo.utils;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/1/19 14:50
+ */
+public enum Status {
+    Success,
+    Fail,
+    Unknow
+}
--- src/test/java/com/frankie/demo/DemoApplicationTests.java
+++ src/test/java/com/frankie/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.frankie.demo;
 
+import com.frankie.demo.utils.Status;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,4 +16,35 @@ class DemoApplicationTests {
         System.out.println("yyc");
     }
 
+    @Test
+    public void switchTest(){
+        Status status = Status.Unknow;
+        switch (status) {
+            default:
+            case Unknow:
+            case Fail:
+                System.out.println("enter fail block");
+                break;
+            case Success:
+                break;
+        }
+        System.out.println("Done");
+    }
+
+    @Test
+    public void switchTest2(){
+        String name = "2";
+
+        switch (name) {
+            case "1":
+                System.out.println("1");
+            case "2":
+                System.out.println("2");
+            case "3":
+                System.out.println("3");
+            case "4":
+                System.out.println("4");
+                break;
+        }
+    }
 }
